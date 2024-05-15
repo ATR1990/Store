b3a0596467a51e85f011b19da8010fb922d14a93
AltynbekovT <tauyekelaltynbekov@gmail.com>
2024-05-15 21:51:45 +0500

    Initial Commit

---
+++ Store/StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreApp.swift
+//  Store
+//
+//  Created by Тауекел on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
